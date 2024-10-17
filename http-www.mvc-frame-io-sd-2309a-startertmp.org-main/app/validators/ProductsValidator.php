1d39422cce1970626ca40ddc32488b20e3625b91
ThomasTadesse <328704@student.mboutrecht.nl>
2024-10-17 13:45:05 +0200

    product validator aangemaakt

---
+++ http-www.mvc-frame-io-sd-2309a-startertmp.org-main/app/validators/ProductsValidator.php
@@ -0,0 +1,52 @@
+<?php
+
+class ProductsValidator
+{
+    public static function validateProductsInputFields($data)
+    {
+        if (empty($data['Product'])) {
+            $data['ProductError'] = "U bent verplicht een product in te vullen";
+        }
+        if (strlen($data['Product']) > 30) {
+            $data['ProductError'] = "Uw product heeft meer letters dan is toegestaan (minder 9 is toegestaan), kies een ander.";
+        }
+
+        if (empty($data['Barcode'])) {
+            $data['BarcodeError'] = "U bent verplicht een barcode in te vullen";
+        }
+        if (strlen($data['Barcode']) != 13) {
+            $data['BarcodeError'] = "De barcode moet precies 13 tekens lang zijn";
+        }
+
+        if (!isset($data['IsActief']) || !is_bool($data['IsActief'])) {
+            $data['IsActiefError'] = "moet kiezen tussen true of false";
+        }
+
+        if (isset($data['Opmerking']) && strlen($data['Opmerking']) > 255) {
+            $data['OpmerkingError'] = "De opmerking mag niet meer dan 255 tekens bevatten";
+        }
+
+        if (isset($data['DatumAangemaakt']) && !strtotime($data['DatumAangemaakt'])) {
+            $data['DatumAangemaaktError'] = "DatumAangemaakt moet een geldige datetime string zijn";
+        }
+
+        if (isset($data['DatumGewijzigd']) && !strtotime($data['DatumGewijzigd'])) {
+            $data['DatumGewijzigdError'] = "DatumGewijzigd moet een geldige datetime string zijn";
+        }
+
+        if (
+            empty($data['ProductError']) 
+            && empty($data['BarcodeError'])
+            && empty($data['IsActiefError'])
+            && empty($data['OpmerkingError'])
+            && empty($data['DatumAangemaaktError'])
+            && empty($data['DatumGewijzigdError'])
+        ) {
+            $data['isValidView'] = true;
+        } else {
+            $data['isValidView'] = false;
+        }
+
+        return $data;
+    }
+}
